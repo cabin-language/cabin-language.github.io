72536934e364922ee9ea0bb82762cde2140eae8b
Violet Iapalucci <ndi26@drexel.edu>
2025-03-05 18:33:28 -0500

    New build deployment

--- _app/immutable/nodes/4.B4PanbjW.js
+++ _app/immutable/nodes/4.B4PanbjW.js
@@ -1,4 +1,4 @@
-import{t as i,a as o}from"../chunks/Do8b9Fsz.js";import"../chunks/BVgGkzlK.js";import{n as s}from"../chunks/BFVeUpzy.js";import{D as n}from"../chunks/Cj5kn2Hk.js";var r=i(`<h1>Welcome</h1> <p>Welcome to Cabin! This documentation is designed for newcomers to the language. It'll walk you
+import"../chunks/CWj6FrbW.js";import"../chunks/69_IOA4Y.js";import{n as i}from"../chunks/F443iEq-.js";import{t as o,a as s}from"../chunks/Nwm3YsBg.js";import{D as n}from"../chunks/MYlD7Bb1.js";var r=o(`<h1>Welcome</h1> <p>Welcome to Cabin! This documentation is designed for newcomers to the language. It'll walk you
 		through the basics of Cabin and how to get started with the language, all the way from your
 		first "Hello world" program to complex programs with external libraries.</p> <h2>What is Cabin?</h2> <p>Cabin is a programming language designed to be <b>safe</b>, <b>simple</b>, and <b>speedy</b>. To
 		be specific, it's a memory-safe, statically typed, compiled, multi-paradigm programming
@@ -8,4 +8,4 @@ import{t as i,a as o}from"../chunks/Do8b9Fsz.js";import"../chunks/BVgGkzlK.js";i
 		to be. This leads to a superb developer experience where complex bugs can be caught at compile-time
 		and displayed in real-time in an editor.</p> <h2>Navigating These Docs</h2> <p>On the left side of these docs you'll see the sidebar with the book's contents. You may notice
 		that it's quite short, and that's because Cabin is quite simple. You could very easily learn the
-		entire language in a day.</p> <h2>Final Remarks</h2> <p>Good luck, and stay cozy!</p>`,1);function d(e){n(e,{page:"Introduction",children:(a,l)=>{var t=r();s(16),o(a,t)},$$slots:{default:!0}})}export{d as component};
+		entire language in a day.</p> <h2>Final Remarks</h2> <p>Good luck, and stay cozy!</p>`,1);function b(e){n(e,{page:"Introduction",children:(a,l)=>{var t=r();i(16),s(a,t)},$$slots:{default:!0}})}export{b as component};
